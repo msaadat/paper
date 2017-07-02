86f65ede96566d6290ce0ce1f20061de12ba2808
Saadat Saeed <saadatsaeed@gmail.com>
2017-07-02 15:30:15 +0500

    added initial markdown highlighting

--- paper_editor.py
+++ paper_editor.py
@@ -1,8 +1,39 @@
 import re
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QTextCursor
+from PyQt5.QtGui import QTextCursor, QSyntaxHighlighter, QTextCharFormat, QFont
 from PyQt5.QtWidgets import QTextEdit
+
+
+class MarkdownHighlighter(QSyntaxHighlighter):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.highlightingRules = []
+
+        headingFormat = QTextCharFormat()
+        defaultSize = parent.defaultFont().pointSize()
+        headingFormat.setFontPointSize(defaultSize * 1.3)
+        headingFormat.setFontWeight(QFont.Bold)
+        self.highlightingRules.append(("^(#+)\\s*(.+?)$", headingFormat))
+
+        italicFormat = QTextCharFormat()
+        italicFormat.setFontItalic(True)
+        self.highlightingRules.append(("\\B\\*{1}(.+?)\\*{1}\\B", italicFormat))
+
+        boldFormat = QTextCharFormat()
+        boldFormat.setFontWeight(QFont.Bold)
+        self.highlightingRules.append(("\\B\\*{2}(.+?)\\*{2}\\B", boldFormat))
+
+        strikeFormat = QTextCharFormat()
+        strikeFormat.setFontStrikeOut(True)
+        self.highlightingRules.append(("\B\~{2}(.+?)\~{2}\B", strikeFormat))
+
+    def highlightBlock(self, text):
+        for pattern, frmt in self.highlightingRules:
+            m = re.match(pattern, text)
+            if m:
+                self.setFormat(m.start(), m.end(), frmt)
 
 
 class PaperEditor(QTextEdit):
@@ -16,6 +47,9 @@ class PaperEditor(QTextEdit):
         if "FontSize" in config['Paper']:
             size = config['Paper']['FontSize']
             self.setFontPointSize(int(size))
+
+        self.document().setDefaultFont(self.currentFont())
+        self.highlighter = MarkdownHighlighter(self.document())
 
     def setDirty(self, status=True):
         self.dirty = status
